aa14f8c7a174fceb2158635e3c36af5de5fa7fee
MO mursi <jemkarry@outlook.com>
2018-09-17 06:42:16 +0000

    automatically backed up by learn

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears (id,name , age ,gender, color,temperature ,alive) VALUES (1,"Mr. Chocolate","M","black",34,TRUE);
+INSERT INTO bears (id,name , age ,gender, color,temperature ,alive) VALUES (2,"Rowdy","M","black",44,TRUE);
+INSERT INTO bears (id,name , age ,gender, color,temperature ,alive) VALUES (3,"Tabitha","F","white",24,TRUE);
+INSERT INTO bears (id,name , age ,gender, color,temperature ,alive) VALUES (4,"Sergeant Brown","M","green",35,TRUE);
+INSERT INTO bears (id,name , age ,gender, color,temperature ,alive) VALUES (5,"Melissa","F","yellow",23,TRUE);
+INSERT INTO bears (id,name , age ,gender, color,temperature ,alive) VALUES (6,"Grinch","M","grey",12,TRUE);
+INSERT INTO bears (id,name , age ,gender, color,temperature ,alive) VALUES (7,"Wendy","F","pink",86,TRUE);
+INSERT INTO bears (id, age ,gender, color,temperature ,alive) VALUES (8,"F","black",32,TRUE);
